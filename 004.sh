85d614eb72c266537f4ed5942e5d14b7e6e1f281
Alejandro <alejandro.ruiz.berciano@gmail.com>
2020-11-19 20:01:35 +0100

    4 y 5

---
+++ 004.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd
+locate Text_example
+cd ~/Data/shell
+rm 002.sh 002.out 003.out 003.sh
+cat -n Text_example.txt > 004.out
---
+++ 005.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+'#!/bin/bash' > 005.out
+echo '#!/bin/bash' > 005.out
+echo "This file is created by $USER on $(date)" | >> 005.out
